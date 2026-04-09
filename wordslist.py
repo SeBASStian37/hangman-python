affaa3d92e5670a1e1938b75ab76266d89472c3b
sebastianschlicht <bassshiny@gmail.com>
2026-04-09 13:57:04 +0200

    hangman in python

---
+++ wordslist.py
@@ -0,0 +1,17 @@
+#words for hangman game
+
+
+words = (
+    "alligator", "antelope", "baboon", "badger", "bass", "bat", "beagle",
+    "bear", "beaver", "bee", "beetle", "bird", "bison", "boar", "buffalo",
+    "bulbul", "bull", "butterfly", "buzzard", "camel", "cardinal", "cat",
+    "catfish", "chameleon", "cheetah", "chihuahua", "chimpanzee", "chipmunk",
+    "colugo", "cotinga", "cougar", "cow", "coyote", "crab", "crocodile",
+    "crow", "deer", "dipper", "dodo", "dog", "dolphin", "donkey",
+    "duck", "duiker", "eagle", "eel", "elephant", "elk", "emu", "falcon",
+    "fish", "flamingo", "fox", "frog", "giraffe", "goat", "goose", "gorilla",
+     "hamster", "hare", "hawk", "hedgehog", "hen",
+    "hippopotamus", "horse", "hummingbird", "hyena", "jackal", "jaguar", "jay",
+    "jellyfish", "kangaroo", "kingfisher", "koala", "lemur", "leopard", "lion",
+    "lizard", "llama", "lynx", "meerkat", "mole", "monkey", "moose", "moth")
+
